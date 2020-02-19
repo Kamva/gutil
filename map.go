2a335f0f6cd0a8ad6b0a6aee0e9733e68ccf78e3
Mehran Prs <mehr.prs@gmail.com>
2020-02-19 14:59:05 +0330

    Add key-value pair to map converter function.

---
+++ map.go
@@ -0,0 +1,20 @@
+package gutil
+
+import (
+	"errors"
+	"fmt"
+)
+
+func KeyValuesToMap(keyValues ...interface{}) (map[string]interface{}, error) {
+	m := make(map[string]interface{})
+
+	for i := 0; i < len(keyValues); i += 2 {
+		if i+1 < len(keyValues) {
+			m[fmt.Sprint(keyValues[i])] = keyValues[i+1]
+		} else {
+			return nil, errors.New(fmt.Sprintf("missing %v index as value", i))
+		}
+	}
+
+	return m, nil
+}
