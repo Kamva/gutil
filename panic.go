f3d8cdcee96d3662571bf2e8a402e4ea73d0ba8d
Mehran Prs <mehr.prs@gmail.com>
2020-02-20 12:36:03 +0330

    Add PanicErr functino.

--- panic.go
+++ panic.go
@@ -1,8 +1,15 @@
 package gutil
 
-// PanicIfNil will panic error param if value is nil.
-func PanicIfNil(val interface{}, err error) {
+// PanicNil will panic error param if value is nil.
+func PanicNil(val interface{}, err error) {
 	if IsNil(val) {
+		panic(err)
+	}
+}
+
+// PanicErr panic if error value is not nil.
+func PanicErr(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
