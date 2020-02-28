7f3374a9b2dbec1695514071febf74ae723b15f9
Mehran Prs <mehr.prs@gmail.com>
2020-02-28 15:36:20 +0330

    Add MapToStruct function

--- struct.go
+++ struct.go
@@ -11,3 +11,13 @@ func StructToMap(input interface{}) map[string]interface{} {
 
 	return m
 }
+
+// MapToStruct convert map to struct by json marshal and Unmarshal
+func MapToStruct(m map[string]interface{}, s interface{}) error {
+	encodedJson, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(encodedJson, s)
+}
