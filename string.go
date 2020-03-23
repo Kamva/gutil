37ef54792117f137bf4833e3a9369a5d06ad02d7
Mehran Prs <mehr.prs@gmail.com>
2020-03-24 01:58:34 +0430

    Add ToSnakeCase fucntion

--- string.go
+++ string.go
@@ -1,5 +1,13 @@
 package gutil
 
+import (
+	"regexp"
+	"strings"
+)
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
 // StringDefault return default value if provided value is empty.
 func StringDefault(val, def string) string {
 	if val == "" {
@@ -7,4 +15,11 @@ func StringDefault(val, def string) string {
 	}
 
 	return val
+}
+
+// ToSnakeCase returns snake_case of the provided value.
+func ToSnakeCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
 }
