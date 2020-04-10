7e8534aaefbeccf59c4f9a8ef4681744f5fded95
Mehran Prs <mehr.prs@gmail.com>
2020-04-10 15:20:54 +0430

    Optimize causeErr fucntion

--- error.go
+++ error.go
@@ -1,5 +1,7 @@
 package gutil
 
+import "errors"
+
 // Must check if error is not nil, so panic, otherwise return value.
 func Must(val interface{}, err error) interface{} {
 	if err != nil {
@@ -20,15 +22,10 @@ func AnyErr(errors ...error) error {
 	return nil
 }
 
+// CauseErr returns the root error by unwrapping the errors chain
 func CauseErr(err error) error {
-	for {
-		u, ok := err.(interface {
-			Unwrap() error
-		})
-		if !ok {
-			break
-		}
-		err = u.Unwrap()
+	for errors.Unwrap(err) != nil {
+		err = errors.Unwrap(err)
 	}
 	return err
 }
