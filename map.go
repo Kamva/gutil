0e1bdfead8e8671916a75f5138ffa6163dd2ed11
Mehran Prs <mehr.prs@gmail.com>
2020-06-13 01:06:39 +0430

    Implement map key extractor

--- map.go
+++ map.go
@@ -36,7 +36,7 @@ func MapToKeyValue(m map[string]interface{}) []interface{} {
 
 // ExtendMap extend dest map by src map.
 // overwrite argument set overwrite policy.
-func ExtendMap(dest,src map[string]interface{}, overwrite bool) {
+func ExtendMap(dest, src map[string]interface{}, overwrite bool) {
 	for key, val := range src {
 		// If key exists in dest and we can not overwrite it, so continue.
 		if _, ok := dest[key]; ok && !overwrite {
@@ -48,7 +48,7 @@ func ExtendMap(dest,src map[string]interface{}, overwrite bool) {
 
 // ExtendMap extend dest map by src map.
 // overwrite argument set overwrite policy.
-func ExtendStrMap(dest,src map[string]string, overwrite bool) {
+func ExtendStrMap(dest, src map[string]string, overwrite bool) {
 	for key, val := range src {
 		// If key exists in dest and we can not overwrite it, so continue.
 		if _, ok := dest[key]; ok && !overwrite {
@@ -56,4 +56,43 @@ func ExtendStrMap(dest,src map[string]string, overwrite bool) {
 		}
 		dest[key] = val
 	}
+}
+
+// MapKeysExtractor extract the map keys recursively.
+type MapKeysExtractor struct {
+	// Depth is depth of the keys. set to 1 to get just first level of keys.
+	Depth int
+
+	// Separator defines separator string between keys on each depth.
+	// e.g if we set "." separator set to "." so we will have a.b for
+	// {a:{b:"hello"}} map.
+	Separator string
+}
+
+func (e MapKeysExtractor) Extract(m map[string]interface{}) []string {
+	return e.extractRecursive(m, "", 1)
+}
+
+func (e MapKeysExtractor) extractRecursive(m map[string]interface{}, prefix string, currentDepth int) []string {
+	if currentDepth > e.Depth {
+		return nil
+	}
+	var keys []string
+	for k, v := range m {
+		path := e.generatePath(prefix, k)
+		keys = append(keys, path)
+		newMap, ok := v.(map[string]interface{})
+		if ok {
+			keys = append(keys, e.extractRecursive(newMap, path, currentDepth+1)...)
+		}
+	}
+	return keys
+}
+
+func (e MapKeysExtractor) generatePath(prefix, key string) string {
+	if prefix == "" {
+		return key
+	}
+
+	return fmt.Sprintf("%s%s%s", prefix, e.Separator, key)
 }
--- map_test.go
+++ map_test.go
@@ -25,10 +25,52 @@ func TestMapToKeyValue(t *testing.T) {
 		"1": "2",
 		"a": "b",
 	}
-	list := []interface{}{ "1", "2","a", "b"}
+	list := []interface{}{"1", "2", "a", "b"}
 
 	result := MapToKeyValue(m)
 
 	assert.Equal(t, list, result)
+}
+
+func TestMapKeysExtractor_Extract(t *testing.T) {
+	m := map[string]interface{}{
+		"a": map[string]interface{}{
+			"b": map[string]interface{}{
+				"d": 1,
+			},
+			"c": 1,
+		},
+	}
+
+	depths := []struct {
+		tag   string
+		depth int
+		keys  []string
+	}{
+		{"t1", 1, []string{"a"}},
+		{"t2", 2, []string{"a", "a.b", "a.c"}},
+		{"t3", 3, []string{"a", "a.b", "a.b.d", "a.c"}},
+		{"t4", 10, []string{"a", "a.b", "a.b.d", "a.c"}},
+	}
+
+	for _, inp := range depths {
+		keys := MapKeysExtractor{Depth: inp.depth, Separator: "."}.Extract(m)
+		assert.Equal(t, inp.keys, keys, inp.tag)
+	}
+
+}
+
+func TestMapKeysExtractor_ExtractInvalidDepth(t *testing.T) {
+	m := map[string]interface{}{
+		"a": map[string]interface{}{
+			"b": map[string]interface{}{
+				"d": 1,
+			},
+			"c": 1,
+		},
+	}
+
+	keys := MapKeysExtractor{Depth: 0, Separator: "."}.Extract(m)
+	assert.Nil(t, keys)
 
 }
