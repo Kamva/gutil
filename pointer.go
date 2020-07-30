aded44383e819cffaa9e6222e452c136b35ec018
Mehran Prs <mehr.prs@gmail.com>
2020-07-30 18:36:50 +0430

    Update pointer the built-in types methods

--- pointer.go
+++ pointer.go
@@ -1,21 +1,6 @@
 package gutil
 
-// BoolPtr returns pointer to the bool value.
-func BoolPtr(b bool) *bool {
-	return &b
-}
-
-// IntPtr returns pointer to the integer value.
-func IntPtr(i int) *int {
-	return &i
-}
-
-// Int64Ptr returns pointer to the int64 value.
-func Int64Ptr(i int64) *int64 {
-	return &i
-}
-
-// StringPtr returns pointer to the string value.
-func StringPtr(s string) *string {
-	return &s
-}
+func NewBool(b bool) *bool       { return &b }
+func NewInt(i int) *int          { return &i }
+func NewInt64(i int64) *int64    { return &i }
+func NewString(s string) *string { return &s }
