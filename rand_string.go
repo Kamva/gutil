2c832a3e3183a9a311216bea15ed3f11a850dfcc
Mehran Prs <mehr.prs@gmail.com>
2020-09-26 20:47:40 +0330

    Update random generator

--- rand_string.go
+++ rand_string.go
@@ -1,34 +1,61 @@
 package gutil
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"math/big"
 )
 
-var randSeeded = false
-var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+type RandType int
 
-func init() {
-	seedRand()
-}
+const (
+	RandTypeAlphaNum RandType = iota + 1
+	RandTypeAlpha
+	RandTypeNumber
+)
 
-func seedRand() {
-	rand.Seed(time.Now().UTC().UnixNano())
-	randSeeded = true
+// RandNumber generate secure random number in range of [min,max).
+func RandNumber(min, max int64) int64 {
+	if min > max {
+		PanicErr(errors.New("min can not be greater than max"))
+	}
+	nBig, err := rand.Int(rand.Reader, big.NewInt(max-min))
+	if err != nil {
+		PanicErr(err)
+	}
+	return nBig.Int64() + min
 }
 
 // RandString returns random string with length of n.
 func RandString(n int) string {
-	if !randSeeded {
-		seedRand()
+	rb := make([]byte, n)
+	_, err := rand.Read(rb)
+
+	if err != nil {
+		panic(err)
 	}
-	return randSeq(n)
+
+	return base64.URLEncoding.EncodeToString(rb)[:n]
 }
 
-func randSeq(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+func RandStringWithType(size int, t RandType) string {
+	var dictionary string
+	switch t {
+	case RandTypeAlphaNum:
+		dictionary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	case RandTypeAlpha:
+		dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	case RandTypeNumber:
+		dictionary = "0123456789"
 	}
-	return string(b)
+
+	var bytes = make([]byte, size)
+	if _, err := rand.Read(bytes); err != nil {
+		panic(err)
+	}
+	for k, v := range bytes {
+		bytes[k] = dictionary[v%byte(len(dictionary))]
+	}
+	return string(bytes)
 }
