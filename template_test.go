6bcd4cd1ef149a105c7dae79401b12c9b2c5d0bb
Mehran Prs <mehr.prs@gmail.com>
2020-10-05 12:12:08 +0330

    Implement text template renderer

---
+++ template_test.go
@@ -0,0 +1,32 @@
+package gutil
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRenderTextTemplate(t *testing.T) {
+	type A struct {
+		Name string `json:"name"`
+	}
+	var testTable = []struct {
+		tag      string
+		text     string
+		data     interface{}
+		result   string
+		hasError bool
+	}{
+		{"t1", "", nil, "", false},
+		{"t2", "hi {{.Name}}", A{Name: "mehran"}, "hi mehran", false},
+		{"t3", "hi {{.LastName}}", map[string]string{"Name": "mehran"}, "hi <no value>", false},
+	}
+	for _, data := range testTable {
+		result, err := RenderText(data.text, data.data)
+		if data.hasError {
+			assert.Error(t, err, data.tag)
+			continue
+		}
+		assert.Nil(t, err)
+		assert.Equal(t, data.result, result, data.tag)
+	}
+}
