2da659923fe142196d047e20ec271f7f1cfd899d
mehran-prs <mehr.prs@gmail.com>
2020-11-03 22:33:26 +0330

    Add string helper functions to replace substring in specific index

--- string.go
+++ string.go
@@ -23,3 +23,15 @@ func ToSnakeCase(str string) string {
 	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 	return strings.ToLower(snake)
 }
+
+// ReplaceAt replace provided string in specific index to another index.
+// removal part is [begin,end).
+// e.g., to replace char "a" in the "salam" word, call ReplaceAt("salam","b",1,2) // result will be "sblam".
+func ReplaceAt(str string, replace string, begin int, end int) string {
+	return str[:begin] + replace + str[end:]
+}
+
+// ReplaceRune replace single rune in specific index
+func ReplaceRune(str string, r rune, removalIndex int) string {
+	return ReplaceAt(str, string(r), removalIndex, removalIndex+1)
+}
