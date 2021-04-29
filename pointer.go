128b9b78e4cf974893621de59375850fa132ea79
mehran-prs <mehr.prs@gmail.com>
2021-04-29 15:14:14 +0430

    Add int32

--- pointer.go
+++ pointer.go
@@ -2,5 +2,6 @@ package gutil
 
 func NewBool(b bool) *bool       { return &b }
 func NewInt(i int) *int          { return &i }
+func NewInt32(i int32) *int32    { return &i }
 func NewInt64(i int64) *int64    { return &i }
 func NewString(s string) *string { return &s }
