826430c4a930803997a28048a9112efc781fde2a
mehran-prs <mehr.prs@gmail.com>
2021-05-18 15:25:27 +0430

    Update bits

--- bits.go
+++ bits.go
@@ -17,3 +17,19 @@ func (b Bits) Toggle(f Bits) Bits {
 func (b Bits) Clear(f Bits) Bits {
 	return b &^ f
 }
+
+func (b Bits) HasInt32(f int32) bool {
+	return b.Has(Bits(f))
+}
+
+func (b Bits) SetInt32(f int32) Bits {
+	return b.Set(Bits(f))
+}
+
+func (b Bits) ToggleInt32(f int32) Bits {
+	return b.Toggle(Bits(f))
+}
+
+func (b Bits) ClearInt32(f int32) Bits {
+	return b.Clear(Bits(f))
+}
